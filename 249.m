6533c121e055419221c4c1c79309c8c134f86e71
Martin Ueding <dev@martin-ueding.de>
2012-08-07 23:06:28 +0200

    Clear

--- 249.m
+++ 249.m
@@ -198,7 +198,11 @@ plot_error = ones(size(plot_y)) * 0;
 function_x = (min(plot_x) : (max(plot_x)-min(plot_x))/10 : max(plot_x))';
 function_y = luft_fit_function(function_x, par);
 
+[plot_y]
+[function_y]
+
 # Erzeuge einen Plot von den Werten.
+clf;
 hold on;
 
 p1 = errorbar(plot_x, plot_y, plot_error, "~.k");
