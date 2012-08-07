bb2a1c4343dcdc13b6ae4660e26ff1f4dcfd3b3c
Martin Ueding <dev@martin-ueding.de>
2012-08-07 18:27:14 +0200

    Add license

--- 249.m
+++ 249.m
@@ -1,6 +1,19 @@
 #!/usr/bin/octave -q
 # Copyright © 2012 Martin Ueding <dev@martin-ueding.de>
 
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 2 of the License, or (at your option) any later
+# version.
+
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+
+# You should have received a copy of the GNU General Public License along with
+# this program. If not, see http://www.gnu.org/licenses/.
+
 ###############################################################################
 #                                  Messwerte                                  #
 ###############################################################################
@@ -8,6 +21,8 @@
 # Lufttemperatur [°C].
 luft.T.val = 20;
 luft.T.err = 0.1;
+
+millikan.
 
 ###############################################################################
 #                               Literaturwerte                                #
